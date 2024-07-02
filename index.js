66f729b0b9f866c0fc87bafcb9caa5a274d80910
Ricardo <emprenet.desarrollo@gmail.com>
2024-07-02 18:04:02 -0300

    Terminado

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
 const puppeteer = require('puppeteer');
 const notifier = require('node-notifier');
 
+const Captcha = require("@2captcha/captcha-solver");
+const solver = new Captcha.Solver("ACA-PONER-APIKEY");
+
+const { setIntervalAsync, clearIntervalAsync } = require('set-interval-async/fixed');
+
+
 (async () => {
+    let encontro_turnos = false;
+    let timer; // Declarar intervalId fuera de las funciones
+
+
+    const ejecutarFuncion = async () => {
+
+        try {
+            console.log('Ejecutando ejecutarFuncion...');
+            console.log('Abriendo navegador...');
+            encontro_turnos = await abrirChromeYHacerTodo();
+            console.log('Resultado de abrirChromeYHacerTodo:', encontro_turnos);
+            if (encontro_turnos && timer) {
+                await clearIntervalAsync(timer);
+                console.log('Se encontraron los turnos. Deteniendo la ejecución.');
+            }
+        } catch (error) {
+            console.error('Error en ejecutarFuncion:', error);
+            console.log('Hubo algún error y explotó todo');
+        }
+
+
+    }
+
+    await ejecutarFuncion()
+
+    timer = setIntervalAsync(async () => {
+        console.log('Hello')        
+        await ejecutarFuncion()
+        console.log('Bye')
+    }, 1000 * 60 * 15); // 15 minutos = 1000 milisegundos x 60 segundos x 15 minutos
+
+
+})();
+
+
+
+async function abrirChromeYHacerTodo()
+{
+
+
     // Launch the browser in desktop mode and maximized
     const browser = await puppeteer.launch({
         headless: false, // to see the browser window
@@ -31,7 +77,7 @@ const notifier = require('node-notifier');
     await page.waitForSelector('#mat-option-6', { visible: true });
     await page.click('#mat-option-6');
 
-    
+
     // Esperar 2 segundos que cargue la pagina en español
     await new Promise(resolve => setTimeout(resolve, 2000)); 
 
@@ -53,7 +99,7 @@ const notifier = require('node-notifier');
     // Hacer click en el boton de Argentina
     await page.evaluate(() => {
         const argentinaLink = Array.from(document.querySelectorAll('li.countries__list__item.ng-star-inserted a'))
-                                  .find(link => link.textContent.trim() === 'ARGENTINA (1)');
+                                .find(link => link.textContent.trim() === 'ARGENTINA (1)');
         if (argentinaLink) {
             argentinaLink.click();
         }
@@ -75,7 +121,7 @@ const notifier = require('node-notifier');
         return items.some(item => item.textContent.trim() === 'Citas en el consulado');
     }, { timeout: 11000 }); // 11 segundos
 
-    
+
 
     // Hacer click en el link de "Tramites de pasaportes"
     await page.evaluate(() => {
@@ -88,7 +134,7 @@ const notifier = require('node-notifier');
 
     // esperar que aparezca "Cita Previa para Pasaporte"
     await page.waitForSelector('#main-content > app-dashboard > app-institutions > app-institutions > app-passport-matters > div > app-passport-appointment-reservation > app-passport-appointment-reservation-form > h1', { visible: true });    
-   
+
 
     await page.waitForSelector('#main-content > app-dashboard > app-institutions > app-institutions > app-passport-matters > div > app-passport-appointment-reservation > app-passport-appointment-reservation-form > div > app-captcha > div > div.d-flex > app-button-control > button', { visible: true });
 
@@ -104,7 +150,7 @@ const notifier = require('node-notifier');
     // busca y hace click en el primer select
     await page.evaluate(async () => {        
         const matLabel = Array.from(document.querySelectorAll('mat-label'))
-                              .find(label => label.textContent.trim() === 'Tipo de servicio');
+                            .find(label => label.textContent.trim() === 'Tipo de servicio');
         if (matLabel) {
             const matFormField = matLabel.nextElementSibling.querySelector('mat-select');            
 
@@ -124,7 +170,7 @@ const notifier = require('node-notifier');
     // busca y hace click en el segundo select
     await page.evaluate(async () => {
         const matLabel = Array.from(document.querySelectorAll('mat-label'))
-                              .find(label => label.textContent.trim() === 'Cita Previa para');
+                            .find(label => label.textContent.trim() === 'Cita Previa para');
         if (matLabel) {
             const matFormField = matLabel.nextElementSibling.querySelector('mat-select');
             if (matFormField) {
@@ -141,8 +187,90 @@ const notifier = require('node-notifier');
     })
 
 
-    // Esperar a que aparezca la imagen con el alt "Verificación de imagen"
-    const captchaImg = await page.waitForSelector('img[alt="Verificación de imagen"]');
+    await autoScroll(page);
+
+
+    let pifioElCaptcha = true
+
+    do {
+
+
+            let pudo_resolver_captcha = false
+            let canti_captchas = 0
+
+            do {
+                pudo_resolver_captcha = await resolverCaptcha(page)
+                console.log('pudo devolver algun captcha? ', pudo_resolver_captcha)
+                canti_captchas = canti_captchas + 1
+                
+            } while (!pudo_resolver_captcha && canti_captchas < 3);
+
+            if(!pudo_resolver_captcha) {
+                console.log('tuvo algun problema en resolver el captcha, cerramos este navegador')
+                await browser.close();
+                return false
+            }
+
+            const botones = await page.evaluate(() => {
+                const but = document.querySelector('#main-content > app-dashboard > app-institutions > app-institutions > app-passport-matters > div > app-passport-appointment-reservation > app-passport-appointment-reservation-form > div > app-captcha > div > div.d-flex > app-button-control > button')
+                but.click()
+            })
+
+
+            await page.waitForNetworkIdle({
+                idleTime: 500, // Tiempo de inactividad en milisegundos
+                timeout: 15000 // Tiempo máximo de espera en milisegundos
+            });    
+
+
+            const pifio = await page.evaluate(() => {
+                return document.body.innerText.includes("Proporcione la solución CAPTCHA");
+            });
+
+            pifioElCaptcha = pifio
+
+            if (pifioElCaptcha) {
+                console.log('le pifio al captcha al parecer....')      
+            } else {
+                console.log('mando captcha y estaba OK...')
+            }            
+
+    } while (pifioElCaptcha)
+
+
+    const noHayCitas = await page.evaluate(() => {
+        return document.body.innerText.includes("No hay citas disponibles. Inténtalo de nuevo más tarde.");
+    });
+
+    if(noHayCitas) {
+        
+        console.log('NO HAY CITAS, HABRA QUE PROBAR OTRA VEZ...')
+        console.log('.....')
+        console.log('.....')
+        console.log('.....')
+        await browser.close();        
+        return false
+
+    } else {
+
+        console.log('---------------ATENCION----------------------')
+        console.log('salio algo distinto a "no hay citas" chequear')
+        console.log('---------------ATENCION----------------------')
+
+        notifier.notify({
+            title: 'Llenador de Campos',
+            message: 'SALIO ALGO DISTINTO A "No hay citas!!" chequearrr',
+            sound: true // optional, adds a sound to the notification
+        });
+        return true
+
+    }
+
+}
+
+async function resolverCaptcha(page)
+{
+
 
     // Esperar hasta que la imagen tenga un atributo src válido
     await page.waitForFunction(() => {
@@ -155,20 +283,64 @@ const notifier = require('node-notifier');
         const img = document.querySelector('img[alt="Verificación de imagen"]');
         return img ? img.src : null;
     });
-
-    console.log('FALTA. El src de la imagen es ya lo tengo pero no lo puedo resolver');
-
-
     
+    let res
+    let texto_captcha = ''
 
-    // Mostrar Notificacion
-    notifier.notify({
-        title: 'Llenador de Campos',
-        message: 'Se llenaron todos los campos.',
-        sound: true // optional, adds a sound to the notification
+
+    console.log('enviando a resolver captcha... ');
+    
+    try {
+        res = await solver.imageCaptcha({
+            body: src,
+            numeric: 0,
+            min_len: 4,
+            max_len: 4,
+            phrase: false,
+            case: true
+        })    
+            
+        texto_captcha = res.data
+
+        console.log('captcha resuelto: ' + texto_captcha)
+
+        await page.evaluate((texto_captcha) => {
+            const textInput = document.querySelector('input[aria-label="Texto que se muestra en la imagen"]');
+            const nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
+            nativeInputValueSetter.call(textInput, texto_captcha);
+            textInput.dispatchEvent(new Event('input', { bubbles: true }));
+        }, texto_captcha);
+
+
+        console.log('captcha resuelto!')
+        
+        return true
+
+    } catch (error) {
+
+        console.log('error al resolver captcha');
+
+        return false
+    }
+
+}
+
+
+async function autoScroll(page) {
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+        var totalHeight = 0;
+        var distance = 100;
+        var timer = setInterval(() => {
+            var scrollHeight = document.body.scrollHeight;
+            window.scrollBy(0, distance);
+            totalHeight += distance;
+    
+            if (totalHeight >= scrollHeight) {
+            clearInterval(timer);
+            resolve();
+            }
+        }, 100);
+        });
     });
-
-    // Keep the browser open
-    // Comment out the next line if you want the browser to stay open
-    // await browser.close();
-})();
+    }
